92c3218951d63fa51f31f9fd675dfbf19bfa2278
Luis Monteiro <109322475+luismonteiromno@users.noreply.github.com>
2025-08-27 22:35:56 -0300

    feat: Inserindo a tabela periodo letivo na model e estruturando as demais tabelas

---
+++ backend/app/models/unigrande.py
@@ -0,0 +1,46 @@
+from tortoise import fields, models
+from datetime import datetime
+
+class PeriodoLetivo(models.Model):
+    id: int = fields.IntField(pk=True)
+    ano: int = fields.IntField()
+    semestre: int = fields.IntField()
+    data_inicio: datetime = fields.DateField()
+    data_fim: datetime = fields.DateField()
+
+    class Meta:
+        table = 'periodos_letivos'
+        unique_together = (('ano', 'semestre'),)
+        indexes = (('ano', 'semestre'),)
+
+
+class Professor(models.Model)
+    pass
+
+
+class Curso(models.Model):
+    pass
+
+
+class Disciplina(models.Model):
+    pass
+
+
+class Matriz(models.Model):
+    pass
+
+
+class Turme(models.Model):
+    pass
+
+
+class Aluno(models.Model):
+    pass
+
+
+class Matricula(models.Model):
+    pass
+
+
+class Historico(models.Model):
+    pass
